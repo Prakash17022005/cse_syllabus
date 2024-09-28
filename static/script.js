5e586a4cc700018fafd200437d93d8da6c2bf801
Prakash17022005 <prakashpoint2005@gmail.com>
2024-09-28 22:00:08 +0530

    gpa commited

--- static/script.js
+++ static/script.js
@@ -1,3 +1,13 @@
+const gradeValues={
+    "O":10,
+    "A+":9,
+    "A":8,
+    "B+":7,
+    "B":6,
+    "C":5
+};
+
+
 async function getSemesterData() {
     const semester=document.getElementById("semester").value;
     if(semester){
@@ -13,6 +23,7 @@ function renderTable(data){
                             <th>Subject Code</th>
                             <th>Subject Name</th>
                             <th>Credits</th>
+                            <th>Grade</th>
                         </tr>`;
 
     data.forEach(subject =>{
@@ -20,11 +31,45 @@ function renderTable(data){
                         <td>${subject.code}</td>
                         <td>${subject.name}</td>
                         <td>${subject.credits}</td>
+                        <td>
+                        <select id=semester>
+                            <option value="O">O</option>
+                            <option value="A+">A+</option>
+                            <option value="A">A</option>
+                            <option value="B+">B+</option>
+                            <option value="B">B</option>
+                            <option value="C">C</option>
+                        </select>
+                        </td>
                       </tr>`;
     });
     table+='</table>';
     document.getElementById("tableContainer").innerHTML=table;
 }
-    
+
+//GPA CALCULATION
+function calculateGPA(){
+    const rows=document.querySelectorAll("#tableContainer table tr");
+    let totalcredits=0;
+    let totalGradePoints=0;
+
+    rows.forEach((row,index)=>{
+        if(index===0) return;
+
+        const credits=parseFloat(row.querySelector('.credits').innerText);
+        const grade=row.querySelector('.grade').value;
+
+        const gradeValue=gradeValues[grade];
+
+        totalcredits += credits;
+        console.log(totalcredits);
+        totalGradePoints += credits * gradeValue;
+    });
+
+    const gpa = totalGradePoints/totalcredits;
+    document.getElementById("gpadiv").innerText='Your GPA is: ${gpa.tofixed(2)}';
+}
+  
+document.getElementById("gpa-button").addEventListener("click",calculateGPA);
 
     
