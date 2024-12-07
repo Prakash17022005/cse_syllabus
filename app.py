8275a1923393a2c79949975a9ede45a9615e100c
Prakash17022005 <prakashpoint2005@gmail.com>
2024-12-07 17:26:41 +0530

    commited

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
+from flask_cors import CORS
+
 
 #flask app
 app=Flask(__name__)
-
+CORS(app)
 #route to app
 @app.route('/')
 def home():
@@ -16,7 +18,7 @@ def get_subjects():
     
     if semester:
         
-        df=pd.read_excel('static\subjects.xlsx',sheet_name=f'semester{semester}')
+        df=pd.read_excel('static/subjects.xlsx',sheet_name=f'semester{semester}')
         
         subjects=df.to_dict(orient='records')
         
